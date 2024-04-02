5cb2c63f465f89dda11a1ee337f995798e25b2d6
Deepak Gupta <deep2468@gmail.com>
2024-04-02 12:47:11 +0530

    1. Project files add

---
+++ src/main/java/com/example/demo/JenkinsGithubIntegrationApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsGithubIntegrationApplication {
+	public static Logger logger = LoggerFactory.getLogger(JenkinsGithubIntegrationApplication.class);
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(JenkinsGithubIntegrationApplication.class, args);
+		logger.info("Application is Up and Running ..");	
+	}
+
+}
